20171c26ae83a348916015b3a7e3352966580636
LixNew <129871802+LixNew2@users.noreply.github.com>
2024-05-06 17:46:14 +0200

    sort

--- core/Back_end/src/home.js
+++ core/Back_end/src/home.js
@@ -14,14 +14,23 @@ username.innerHTML = "Connecté en tant que : " + "<b>" + username_local_storage
 
 //Search
 const search_input = document.getElementById("search_input");
+const type = document.getElementById("type");
 
 search_input.addEventListener('input', function(){
     var value = search_input.value.toLowerCase();
+    var type_value = type.value.toLowerCase();
     bids_section.innerHTML = '';
-    get_bid(value);
+    get_bid(value, type_value);
 });
 
-function get_bid(value){
+type.addEventListener('change', function(){
+    var value = search_input.value.toLowerCase();
+    var type_value = type.value.toLowerCase();
+    bids_section.innerHTML = '';
+    get_bid(value, type_value);
+});
+
+function get_bid(value, type){
     read('bids/')
     .then((data) => {
         
@@ -29,40 +38,43 @@ function get_bid(value){
             if(data[key] != "null"){
                 if(data[key].timestamp_end_date >= Date.now()){
                     if((data[key].title.toLowerCase()).startsWith(value) || value == null){
-                        let bid_id = key;
+                        if(data[key].type.toLowerCase() == type || type == "Tout les produits"){
 
-                        var article = document.createElement('article');
-                        article.classList.add('auction_item');
-                        var img = document.createElement('img');
-                        img.src = data[key].img_URL;
-                        article.appendChild(img);
-            
-                        var div = document.createElement('div');
-                        div.classList.add('auction_info');
-                        var h3 = document.createElement('h3');
-                        h3.innerHTML = data[key].title;
-                        div.appendChild(h3);
-                        var p = document.createElement('p');
-                        var end_date = new Date(parseInt(data[key].timestamp_end_date));
-                        var months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
-                        var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
-                        p.innerHTML = "Fin de l'enchère le : " + (days[end_date.getDay()] + " " + end_date.getDate() + " " + months[end_date.getMonth()] + " " + end_date.getFullYear());
-                        div.appendChild(p);
-                        var p = document.createElement('p');
-                        p.innerHTML = "Prix actuel : " + data[key].bid_price + "€";
-                        div.appendChild(p);
-                        var button = document.createElement('button');
-                        button.classList.add('view_auction');
-                        button.id = "view_bids";
-                        button.innerHTML = "Voir l'enchère";
-                        div.appendChild(button);
-                        article.appendChild(div);
-            
-                        bids_section.appendChild(article);
-                        
-                        button.onclick = function(){
-                            localStorage.setItem('bid_id', bid_id);
-                            document.location.href = "./viewbids.html";
+                            let bid_id = key;
+    
+                            var article = document.createElement('article');
+                            article.classList.add('auction_item');
+                            var img = document.createElement('img');
+                            img.src = data[key].img_URL;
+                            article.appendChild(img);
+                
+                            var div = document.createElement('div');
+                            div.classList.add('auction_info');
+                            var h3 = document.createElement('h3');
+                            h3.innerHTML = data[key].title;
+                            div.appendChild(h3);
+                            var p = document.createElement('p');
+                            var end_date = new Date(parseInt(data[key].timestamp_end_date));
+                            var months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+                            var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+                            p.innerHTML = "Fin de l'enchère le : " + (days[end_date.getDay()] + " " + end_date.getDate() + " " + months[end_date.getMonth()] + " " + end_date.getFullYear());
+                            div.appendChild(p);
+                            var p = document.createElement('p');
+                            p.innerHTML = "Prix actuel : " + data[key].bid_price + "€";
+                            div.appendChild(p);
+                            var button = document.createElement('button');
+                            button.classList.add('view_auction');
+                            button.id = "view_bids";
+                            button.innerHTML = "Voir l'enchère";
+                            div.appendChild(button);
+                            article.appendChild(div);
+                
+                            bids_section.appendChild(article);
+                            
+                            button.onclick = function(){
+                                localStorage.setItem('bid_id', bid_id);
+                                document.location.href = "./viewbids.html";
+                            }
                         }
                     }
                     
@@ -77,4 +89,4 @@ function get_bid(value){
     });
 }
 
-get_bid();
+get_bid(null, "Tout les produits");
